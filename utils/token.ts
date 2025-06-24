03cf69e4c402e5ccda47de1f9b67ac9b0673243e
FarizioKautsar <kheruzy@gmail.com>
2025-06-24 17:31:10 +1000

    Token can work in web

--- utils/token.ts
+++ utils/token.ts
@@ -1,13 +1,28 @@
+// src/utils/token.ts
+import { Platform } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const TOKEN_KEY = 'access_token';
+
+const isWeb = Platform.OS === 'web';
 
 export const saveToken = async (token: string) => {
-  await SecureStore.setItemAsync('access_token', token);
+  if (isWeb) {
+    await AsyncStorage.setItem(TOKEN_KEY, token);
+  } else {
+    await SecureStore.setItemAsync(TOKEN_KEY, token);
+  }
 };
 
 export const getToken = async () => {
-  return await SecureStore.getItemAsync('access_token');
+  return isWeb
+    ? await AsyncStorage.getItem(TOKEN_KEY)
+    : await SecureStore.getItemAsync(TOKEN_KEY);
 };
 
 export const deleteToken = async () => {
-  await SecureStore.deleteItemAsync('access_token');
+  return isWeb
+    ? await AsyncStorage.removeItem(TOKEN_KEY)
+    : await SecureStore.deleteItemAsync(TOKEN_KEY);
 };
