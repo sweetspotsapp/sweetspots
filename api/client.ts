7851e308424344af7d0ca75acc35be5bd5f4841f
FarizioKautsar <kheruzy@gmail.com>
2025-07-23 11:48:29 +1000

    Just use the sam eurl for both mobilr and web

--- api/client.ts
+++ api/client.ts
@@ -9,12 +9,11 @@ const isDev = process.env.NODE_ENV === 'development';
 // Replace this with your local machine's IP address
 
 let baseURL = 'https://your-prod-api.com/api/v1';
-export const MOBILE_URL = 'http://192.168.4.21:8080'
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'http://192.168.4.21:8080';
 
 if (isDev) {
   if (Platform.OS === 'ios' || Platform.OS === 'android') {
-    baseURL = `${MOBILE_URL}/api/v1`;
+    baseURL = `${API_URL}/api/v1`;
   } else {
     // for web
     baseURL = `${API_URL}/api/v1`;
