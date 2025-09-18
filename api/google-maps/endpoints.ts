5a937cf99e094101a13aabf76f378ca4b548c555
FarizioKautsar <kheruzy@gmail.com>
2025-09-18 12:17:02 +1000

    getPlaceCoordinates endpoint

--- api/google-maps/endpoints.ts
+++ api/google-maps/endpoints.ts
@@ -1,10 +1,22 @@
 import { api } from '../client';
 import { ApiResponse } from '../pagination.dto';
-import { AutocompleteCitiesQueryDto, CitySuggestionDto } from '@/dto/google-maps/autocomplete-cities.dto';
+import {
+  AutocompleteCitiesQueryDto,
+  CitySuggestionDto,
+} from '@/dto/google-maps/autocomplete-cities.dto';
 
 export const getAutocompleteCities = async (
   params?: AutocompleteCitiesQueryDto
 ): Promise<ApiResponse<{ suggestions: CitySuggestionDto[] }>> => {
   const res = await api.get('/maps/autocomplete-cities', { params });
+  return res.data;
+};
+
+export const getPlaceCoordinates = async (
+  placeId: string
+): Promise<
+  ApiResponse<{ latitude: number; longitude: number; formattedAddress: string }>
+> => {
+  const res = await api.get(`/maps/place-coordinates/${placeId}`);
   return res.data;
 };
