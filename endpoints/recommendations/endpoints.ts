6159d715ca9b1eb649f85152bbc885d91a15a5c1
FarizioKautsar <kheruzy@gmail.com>
2025-10-04 13:46:28 +1000

    getRecContext endpoint

--- endpoints/recommendations/endpoints.ts
+++ endpoints/recommendations/endpoints.ts
@@ -3,6 +3,7 @@ import { ApiPluralResponse, ApiResponse } from '../pagination.dto';
 import { GetRecommendationsDto } from '../../dto/recommendations/get-recommendations.dto';
 import { IRecommendedPlace } from '../../dto/recommendations/recommendation.dto';
 import { UpdatePreferencesDto } from '../../dto/recommendations/update-preferences.dto';
+import { GetRecContextDto } from '@/dto/recommendations/get-rec-context.dto';
 
 export const getRecommendations = async (
   params?: GetRecommendationsDto
@@ -15,5 +16,10 @@ export const updateTravelPreferences = async (
   data: UpdatePreferencesDto
 ): Promise<ApiResponse<{ success: boolean }>> => {
   const res = await api.post('/recommendations/preferences', data);
+  return res.data;
+};
+
+export const getRecContext = async (data: GetRecContextDto): Promise<ApiResponse<string>> => {
+  const res = await api.get('/recommendations/context', { params: data });
   return res.data;
 };
