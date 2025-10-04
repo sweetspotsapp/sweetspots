1508d4fe419327de4215d92f0b3365b7d2a77f61
FarizioKautsar <kheruzy@gmail.com>
2025-10-04 11:28:09 +1000

    Get onboarding

--- endpoints/users-onboarding/endpoints.ts
+++ endpoints/users-onboarding/endpoints.ts
@@ -1,8 +1,8 @@
 import { api } from '../client';
 import { ApiResponse } from '../pagination.dto';
 export const getMyOnboarding = async () => {
-    const res = await api.get('/onboarding/me');
-    return res.data;
+  const res = await api.get('/onboarding/me');
+  return res.data;
 };
 
 export type UpsertOnboardingPayload = {
@@ -17,17 +17,19 @@ export type UpsertOnboardingPayload = {
   tutorialCompleted?: boolean;
 };
 
-export const upsertMyOnboarding = async (data: UpsertOnboardingPayload): Promise<ApiResponse<any>> => {
-    const res = await api.put('/onboarding/me', data);
-    return res.data;
-}
+export const upsertMyOnboarding = async (
+  data: UpsertOnboardingPayload
+): Promise<ApiResponse<any>> => {
+  const res = await api.put('/onboarding/me', data);
+  return res.data;
+};
 
 export const markOnboardingCompleted = async () => {
-    const res = await api.patch('/onboarding/me/complete');
-    return res.data;
-}
+  const res = await api.patch('/onboarding/me/complete');
+  return res.data;
+};
 
 export const markOnboardingDismissed = async () => {
-    const res = await api.patch('/onboarding/me/dismiss');
-    return res.data;
-}
+  const res = await api.patch('/onboarding/me/dismiss');
+  return res.data;
+};
