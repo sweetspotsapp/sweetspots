01ff8e5d625ffc5986e8ef1876a83bf398d27648
FarizioKautsar <kheruzy@gmail.com>
2025-10-23 08:32:36 +1100

    SSSlider fix

---
+++ components/ui/SSSlider.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Slider, { SliderProps } from '@react-native-community/slider';
+
+export interface SSSliderProps extends SliderProps {}
+
+const SSSlider: React.FC<SSSliderProps> = (props) => {
+  const {
+    style,
+    minimumTrackTintColor = '#f97316',
+    maximumTrackTintColor = '#e2e8f0',
+    ...rest
+  } = props;
+
+  return (
+    <Slider
+      style={[{ width: '100%', height: 40, marginTop: 8 }, style]}
+      {...rest}
+    />
+  );
+};
+
+export default SSSlider;
